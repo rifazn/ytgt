5a38299b3961d005fca5e3a8fda18fe3cb985bb3
Rifaz Nahiyan <rifazn@protonmail.com>
2020-03-14 01:24:16 +0600

    Downloads audio/video. Very very alpha beta.

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import subprocess
 
 app = Flask(__name__)
+
+DL_DIR = "downloads"
+AUDIO_DIR = DL_DIR + "/audio"
+VIDEO_DIR = DL_DIR + "/video"
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route("/dl")
+@app.route("/dl", methods=["GET"])
 def download():
-    return 'OK'
+    # Fetch the form data
+    url = request.args.get("url", None)
+    dlformat = request.args.get("format", None)
+    
+    if url:
+        if dlformat == "audio":
+            status_code = subprocess.call(["youtube-dl", "-x", url])
+            if status_code == 0:
+                return 'OK'
+        elif dlformat == "video":
+            status_code = subprocess.call(["youtube-dl", url])
+            if status_code == 0:
+                return 'OK'
+    return 'NOT OK'
